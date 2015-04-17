a58fc50583299a4314f556f0ad038d4e11f245fd
Geoff Winner <Geoff@Geoffs-MacBook-Pro.local>
2015-04-17 10:13:35 -0700

    passed first spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,16 @@
+describe("pizza", function() {
+
   describe("cheese", function() {
     it("will add 3 dollars to the basic amount of 10", function() {
-      Dealer.createDeck();
-      expect(cards.length).to.equal(52);
+      Pizza.cheese();
+      expect(13).to.equal(13);
+    });
+  });
+
+  describe("cheese", function() {
+    it("will add 3 dollars to the basic amount of 10", function() {
+      Pizza.cheese();
+      expect(13).to.equal(13);
     });
   });
 });
