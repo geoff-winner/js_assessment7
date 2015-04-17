866abc9ee578014aabc6b7f76fd1fa771c8c1425
Geoff Winner <Geoff@Geoffs-MacBook-Pro.local>
2015-04-17 10:11:16 -0700

    wrote first spec

---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+  describe("cheese", function() {
+    it("will add 3 dollars to the basic amount of 10", function() {
+      Dealer.createDeck();
+      expect(cards.length).to.equal(52);
+    });
+  });
+});
